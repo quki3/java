e658084abf7187d074a2ba56b4cda558fd893e91
gaBriel nesTor bruFau <70489784+qukiiignb@users.noreply.github.com>
2023-06-27 09:12:39 -0300

    Update HelloWorldTest.java

--- UNLOCK-SOLUTIONS/1/A/HelloWorldTest.java
+++ UNLOCK-SOLUTIONS/1/A/HelloWorldTest.java
@@ -1,4 +1,7 @@
-import org.junit.Test;
+// import - when you want to used a package external
+// org.junit.jupiter.api.Test - this is the library that we want use all core @ Test is here 
+// org.junit.AssertEquals - we want to use this here
+import org.junit.jupiter.api.Test
 import static org.junit.Assert.assertEquals;
 
 public class HelloWorldTest {
