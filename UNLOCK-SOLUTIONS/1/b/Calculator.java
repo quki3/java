72aff1e673f6a11287a7c5b50fc2d3a954069de4
gaBriel nesTor bruFau <70489784+qukiiignb@users.noreply.github.com>
2023-07-03 11:41:59 -0300

    Update Calculator.java

--- UNLOCK-SOLUTIONS/1/b/Calculator.java
+++ UNLOCK-SOLUTIONS/1/b/Calculator.java
@@ -1,8 +1,10 @@
 public class Calculator {
   public int evaluate(String expression) {
     int sum = 0;
+    // for - bucle for
                                   // split - which splits the string at each occurrence of the + character. The resulting substrings (summands) are stored in an array.
     for (String summand: expression.split("\\+"))
+             //Interger.valueOf - converts a specified input into an Integer
       sum += Integer.valueOf(summand);
     return sum;
   }
